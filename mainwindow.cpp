4cde53a4142df655ec3215a7989fecb4301eac23
four4tec <f74031132@mail.ncku.edu.tw>
2015-05-27 16:18:16 +0800

    proj2 5/27 16:17

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -10,35 +10,69 @@
 #include <iostream>
 #include <ctime>
 #include <QFont>
+#include <unistd.h>
 using namespace std;
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
+    //ui
     ui->setupUi(this);
     setWindowTitle("2048 demo - by fourtec");
     setFixedSize(500,650);
+    //variable
     memset(value,0,sizeof(value));
+    grade=0;
+    end=0;
     for(int i=0;i<16;i++)
     {
         place[i%4][i/4].x=i%4*120+20;
         place[i%4][i/4].y=i/4*120+20;
     }
+    //label*16
     for(int i=0;i<16;i++)
     {
         label[i%4][i/4]=new QLabel(centralWidget());
         label[i%4][i/4]->setGeometry(place[i%4][i/4].x,place[i%4][i/4].y,100,100);
         label[i%4][i/4]->setPixmap(QPixmap(":numbers/num/0.jpg"));
     }
+    //button q
     quit=new QPushButton(centralWidget());
-    quit->setGeometry(370,500,75,35);
+    quit->setGeometry(425,500,60,35);
     quit->setText("Quit");
     quit->setFont(QFont("Courier",12,QFont::Bold));
     quit->setFocusPolicy(Qt::NoFocus);
     connect(quit,SIGNAL(clicked()),qApp,SLOT(quit()));
+    //button r
+    restart=new QPushButton(centralWidget());
+    restart->setGeometry(335,500,80,35);
+    restart->setText("restart");
+    restart->setFont(QFont("Courier",12,QFont::Bold));
+    restart->setFocusPolicy(Qt::NoFocus);
+    connect(restart,SIGNAL(clicked()),this,SLOT(retry()));
+    //label end
+    endlabel=new QLabel(centralWidget());
+    endlabel->setGeometry(25,500,200,50);
+    endlabel->setFont(QFont("Courier",22,QFont::Bold));
+    endlabel->hide();
+    //label grade
+    gradelabel=new QLabel(centralWidget());
+    gradelabel->setGeometry(25,550,400,50);
+    gradelabel->setFont(QFont("Courier",22,QFont::Bold));
+    QString str=QString::number(grade);
+    gradelabel->setText("Grade : "+str);
+    //button laststep
+    laststep=new QPushButton(centralWidget());
+    laststep->setGeometry(225,500,100,35);
+    laststep->setFocusPolicy(Qt::NoFocus);
+    laststep->setFont(QFont("Courier",10,QFont::Bold));
+    laststep->setText("Last step");
+    connect(laststep,SIGNAL(clicked()),this,SLOT(steplast()));
+    //setpic
     randomnum();
     setpic();
+    storestep();
 }
 
 MainWindow::~MainWindow()
@@ -48,12 +82,15 @@ MainWindow::~MainWindow()
 
 void MainWindow::keyPressEvent(QKeyEvent *event)
 {
-    if(event->key()==Qt::Key_Right){pressright(0);}
-    else if(event->key()==Qt::Key_Left){pressleft(0);}
-    else if(event->key()==Qt::Key_Up){pressup(0);}
-    else if(event->key()==Qt::Key_Down){pressdown(0);}
+    if(end){return;}
+    for(int i=0;i<16;i++){if(value[i%4][i/4]==2048){win();}}
+    if(event->key()==Qt::Key_Right){storestep();pressright(0);}
+    else if(event->key()==Qt::Key_Left){storestep();pressleft(0);}
+    else if(event->key()==Qt::Key_Up){storestep();pressup(0);}
+    else if(event->key()==Qt::Key_Down){storestep();pressdown(0);}
     else{return;}
-
+    QString str=QString::number(grade);
+    gradelabel->setText("Grade : "+str);
 }
 int MainWindow::pressup(int mode)
 {
@@ -61,7 +98,7 @@ int MainWindow::pressup(int mode)
     for(i=0;i<16;i++){cpvalue[i]=value[i%4][i/4];}
     for(i=0;i<4;i++){for(j=0;j<3;j++){
         for(int i=0;i<4;i++){for(int j=2;j>=0;j--){if(value[i][j]==0){value[i][j]=value[i][j+1];value[i][j+1]=0;}}}
-        if(value[i][j]==value[i][j+1]){value[i][j]*=2;value[i][j+1]=0;}
+        if(value[i][j]==value[i][j+1]){value[i][j]*=2;value[i][j+1]=0;if(!mode){grade+=value[i][j];}}
     }}
     for(i=0;i<16;i++){if(cpvalue[i]==value[i%4][i/4]){chk++;}}
     if(chk>=16){return 1;}
@@ -79,7 +116,7 @@ int MainWindow::pressdown(int mode)
     for(i=0;i<16;i++){cpvalue[i]=value[i%4][i/4];}
     for(i=0;i<4;i++){for(j=3;j>0;j--){
         for(int i=0;i<4;i++){for(int j=1;j<4;j++){if(value[i][j]==0){value[i][j]=value[i][j-1];value[i][j-1]=0;}}}
-        if(value[i][j]==value[i][j-1]){value[i][j]*=2;value[i][j-1]=0;}
+        if(value[i][j]==value[i][j-1]){value[i][j]*=2;value[i][j-1]=0;if(!mode){grade+=value[i][j];}}
     }}
     for(i=0;i<16;i++){if(cpvalue[i]==value[i%4][i/4]){chk++;}}
     if(chk>=16){return 1;}
@@ -97,7 +134,7 @@ int MainWindow::pressleft(int mode)
     for(i=0;i<16;i++){cpvalue[i]=value[i%4][i/4];}
     for(i=0;i<3;i++){for(j=0;j<4;j++){
         for(int i=2;i>=0;i--){for(int j=0;j<4;j++){if(value[i][j]==0){value[i][j]=value[i+1][j];value[i+1][j]=0;}}}
-        if(value[i][j]==value[i+1][j]){value[i][j]*=2;value[i+1][j]=0;}
+        if(value[i][j]==value[i+1][j]){value[i][j]*=2;value[i+1][j]=0;if(!mode){grade+=value[i][j];}}
     }}
     for(i=0;i<16;i++){if(cpvalue[i]==value[i%4][i/4]){chk++;}}
     if(chk>=16){return 1;}
@@ -115,7 +152,7 @@ int MainWindow::pressright(int mode)
     for(i=0;i<16;i++){cpvalue[i]=value[i%4][i/4];}
     for(i=3;i>0;i--){for(j=0;j<4;j++){
         for(int i=1;i<4;i++){for(int j=0;j<4;j++){if(value[i][j]==0){value[i][j]=value[i-1][j];value[i-1][j]=0;}}}
-        if(value[i][j]==value[i-1][j]){value[i][j]*=2;value[i-1][j]=0;}
+        if(value[i][j]==value[i-1][j]){value[i][j]*=2;value[i-1][j]=0;if(!mode){grade+=value[i][j];}}
     }}
     for(i=0;i<16;i++){if(cpvalue[i]==value[i%4][i/4]){chk++;}}
     if(chk>=16){return 1;}
@@ -144,7 +181,9 @@ int MainWindow::setpic()
 {
     for(int i=0;i<16;i++)
     {
-        switch(value[i%4][i/4])
+        QString str=QString::number(value[i%4][i/4]);
+        label[i%4][i/4]->setPixmap(":numbers/num/"+str+".jpg");
+        /*switch(value[i%4][i/4])
         {
             case 0:label[i%4][i/4]->setPixmap(QPixmap(":numbers/num/0.jpg"));break;
             case 2:label[i%4][i/4]->setPixmap(QPixmap(":numbers/num/2.jpg"));break;
@@ -158,15 +197,12 @@ int MainWindow::setpic()
             case 512:label[i%4][i/4]->setPixmap(QPixmap(":numbers/num/512.jpg"));break;
             case 1024:label[i%4][i/4]->setPixmap(QPixmap(":numbers/num/1024.jpg"));break;
             case 2048:label[i%4][i/4]->setPixmap(QPixmap(":numbers/num/2048.jpg"));break;
-        }
+        }*/
     }
     return 0;
 }
 int MainWindow::testend()
 {
-    QLabel *endlabel=new QLabel(centralWidget());
-    endlabel->setGeometry(150,200,200,100);
-    endlabel->show();
     int i,cpvalue[16],ans[4]={0};
     for(i=0;i<16;i++){cpvalue[i]=value[i%4][i/4];}
     ans[0]=pressup(1);
@@ -179,11 +215,59 @@ int MainWindow::testend()
     for(i=0;i<16;i++){value[i%4][i/4]=cpvalue[i];}
     if(ans[0] && ans[1] && ans[2] && ans[3])
     {
-        QLabel *endlabel=new QLabel("You lose...",centralWidget());
-        endlabel->setGeometry(50,470,200,100);
-        endlabel->setFont(QFont("Courier",22,QFont::Bold));
+        endlabel->setText("You lose...");
         endlabel->show();
+        end=1;
         return 1;
     }
+    return 0;
+}
+int MainWindow::retry()
+{
+    restart->setText("nope!");
+    //time_t t1=time(NULL),t2=time(NULL);
+    //while(t2-t1<3){t2=time(NULL);}
+    memset(value,0,sizeof(value));
+    end=0;
+    randomnum();
+    setpic();
+    restart->setText("restart");
+    grade=0;
+    last.clear();
+    storestep();
+    QString str=QString::number(grade);
+    gradelabel->setText("Grade : "+str);
+    endlabel->hide();
+    return 0;
+}
+int MainWindow::win()
+{
+    endlabel->setText("You win!!!");
+    endlabel->show();
+    end=1;
+    return 0;
+}
+int MainWindow::steplast()
+{
+    end=0;
+    if(!last.isEmpty())
+    {
+        endlabel->hide();
+        storevalue v=last.top();
+        last.pop();
+        for(int i=0;i<16;i++){value[i%4][i/4]=v.vvalue[i];}
+        grade=v.vgrade;
+        QString str=QString::number(grade);
+        gradelabel->setText("Grade : "+str);
+        setpic();
+    }
+    return 0;
+}
+int MainWindow::storestep()
+{
+    storevalue v;
+    for(int i=0;i<16;i++){v.vvalue[i]=value[i%4][i/4];}
+    v.vgrade=grade;
+    last.push(v);
     return 0;
 }
--- mainwindow.h
+++ mainwindow.h
@@ -4,6 +4,8 @@
 #include <QMainWindow>
 #include <QLabel>
 #include <QPushButton>
+#include <Qstack>
+//typedef struct ns{int vvalue[16];}v;
 
 namespace Ui {
 class MainWindow;
@@ -17,20 +19,36 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    int retry();
+    int steplast();
+
 private:
+    //?
     Ui::MainWindow *ui;
+    QLabel *label[4][4];
+    QLabel *endlabel;
+    QLabel *gradelabel;
+    QPushButton *laststep;
+    QPushButton *quit,*restart;
+    //variable
+    struct pplace{int x,y;}place[4][4];
+    int value[4][4];
+    int grade;
+    bool end;
+    typedef struct ns{int vvalue[16],vgrade;}storevalue;
+    QStack<storevalue>last;
+    //function
     void keyPressEvent(QKeyEvent * event);
     int pressup(int);
     int pressdown(int);
     int pressleft(int);
     int pressright(int);
-    struct pplace{int x,y;}place[4][4];
-    int value[4][4];
-    QLabel *label[4][4];
     int randomnum();
     int setpic();
     int testend();
-    QPushButton *quit;
+    int win();
+    int storestep();
 };
 
 #endif // MAINWINDOW_H
